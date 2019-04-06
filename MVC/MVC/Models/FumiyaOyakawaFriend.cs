94ff6d21a63b45d2df9c24d3b8aaea28d26ca11f
fumiya <fumiya.om@gmail.com>
2019-04-06 12:52:56 -0400

    se crearon los modelos para el mvc

---
+++ MVC/MVC/Models/FumiyaOyakawaFriend.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public enum FriendType
+    {
+        Conocido,
+        CompañeroEstudio,
+        ColegaDeTrabajo,
+        Amigo,
+        AmigoDeInfancia,
+        Pariente
+
+    }
+
+    public class FumiyaOyakawaFriend
+    {
+        [Key]
+        public int FriendId { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public FriendType Friend { get; set; }
+        [Required]
+        public DataType Birthdate { get; set; }
+    }
+}
